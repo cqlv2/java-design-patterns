e2611cea15b88cc211b7b5c535b7327dc4877ab8
sylvain amianto <samianto@gmail.com>
2020-12-11 11:34:44 +0100

    test ok

--- src/test/java/fr/diginamic/builder/ZooBuilderTest.java
+++ src/test/java/fr/diginamic/builder/ZooBuilderTest.java
@@ -30,20 +30,17 @@ public class ZooBuilderTest {
 
 	}
 	
-	@Test
-	public void test2() {
+	@Test(expected=Exception.class)
+	public void test2() throws ZoneException {
 		ZooBuilder zb=new ZooBuilder("test2");
 		Zoo zooB=null;
 
 		
-		try {
-			zooB=zb.appendZone("zoneB", 1).appendAnimal("zoneB", new Lion()).appendAnimal("zoneB", new Lion()).get();
-		} catch (ZoneException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		assertEquals("test", zooB.getNom());
+			zooB=zb.appendZone("zoneB", 1).appendAnimal("zoneB", new Lion()).appendAnimal("zoneB", new Lion()).get();
+		
+		
+		assertEquals("test2", zooB.getNom());
 		assertEquals(1, zooB.getZones().size());
 		assertEquals(1, zooB.getZones().get(0).getAnimal().size());
 
